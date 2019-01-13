859c9187422b3c7cb225f47238c29837067507ca
skarimo <karimovj@gmail.com>
2019-01-13 18:12:57 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <BrowserRouter>
+        <React.Fragment>
+          <Switch>
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/projects" component={Projects} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Comtact = (props) => {
     return (
-      <div>Comtact page</div>
+      <div>Contact page</div>
     )
   }
 
