5f1639e1838c5fec443cfd9bf62871e92e4f7be3
skarimo <karimovj@gmail.com>
2019-01-15 15:11:55 -0500

    added icons and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { AnimatedSwitch } from 'react-router-transition'
 
 import Home from './components/Home'
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import gitHubIcon from './icons/github-icon.png'
+import linkedInIcon from './icons/linkedin-icon.png'
+import mediumIcon from './icons/medium-icon.png'
+
 const Header = (props) => {
     return (
-      <header>
-        <h1>Sherzod Karimov</h1>
-          <nav className='menu'>
+      <header id='header'>
+        <h1>Sherzod</h1>
+          <nav className='main-menu'>
             <Link to={`/`}>Home</Link>
             <Link to={`/projects`}>Projects</Link>
             <Link to={`/contact`}>Contact</Link>
           </nav>
+
+          <div class="sidenav">
+            <img className="social-logo" src={linkedInIcon} alt="linkedin-logo"/>
+            <img className="social-logo" src={mediumIcon} alt="medium-logo"/>
+            <img className="social-logo" src={gitHubIcon} alt="github-logo"/>
+          </div>
       </header>
     )
   }
